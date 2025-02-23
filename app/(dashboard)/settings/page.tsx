f8583a40b2c3620a620a8525d5a54f0d4fa1d0a3
Louis-Émile Vromet <levromet5@gmail.com>
2025-02-23 15:26:34 -0500

    fix: settings coming soon

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
     <div>
-      <h1>Settings</h1>
+      <h1>Settings coming soon</h1>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
